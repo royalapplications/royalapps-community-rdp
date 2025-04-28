2f3cc72d30e7b38e227b28ca86557ac270a57d28
Stefan Koell <stefan.koell@royalapps.com>
2025-04-28 20:28:27 +0200

    Remove using

--- src/RoyalApps.Community.Rdp.WinForms/Controls/RdpControl.cs
+++ src/RoyalApps.Community.Rdp.WinForms/Controls/RdpControl.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.IO;
 using System.Threading;

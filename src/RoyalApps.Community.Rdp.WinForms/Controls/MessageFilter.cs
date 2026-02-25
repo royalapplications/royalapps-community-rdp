156ed438b9e38a7680d8e91cffa441665dd0c2a4
Stefan Koell <stefan.koell@royalapps.com>
2026-02-25 14:53:28 +0100

    Avoid synchronous callbacks/focus changes in hook/WndProc paths, because they can trigger re-entrant COM calls on the UI STA and freeze the app.

--- src/RoyalApps.Community.Rdp.WinForms/Controls/MessageFilter.cs
+++ src/RoyalApps.Community.Rdp.WinForms/Controls/MessageFilter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Forms;
 using Windows.Win32;
 using RoyalApps.Community.Rdp.WinForms.Interfaces;
@@ -6,6 +7,9 @@ namespace RoyalApps.Community.Rdp.WinForms.Controls;
 
 internal static class MessageFilter
 {
+    [ThreadStatic]
+    private static bool _isProcessingClickMessage;
+
     public static bool Filter(IRdpClient control, ref Message m)
     {
         // processing base.WndProc(m) when WM_DESTROY is received while the control has already been disposed
@@ -18,10 +22,46 @@ internal static class MessageFilter
              m.Msg != PInvoke.WM_MDIACTIVATE))
             return false;
 
-        // notify host application that the RDP client area has been clicked
-        control.RaiseClientAreaClicked();
-        if (!control.ContainsFocus)
-            control.Focus();
+        if (_isProcessingClickMessage)
+            return true;
+
+        // Avoid synchronous callbacks/focus changes in hook/WndProc paths, because
+        // they can trigger re-entrant COM calls on the UI STA and freeze the app.
+        if (control is Control winFormsControl && control.IsHandleCreated && !control.IsDisposed)
+        {
+            _isProcessingClickMessage = true;
+            try
+            {
+                winFormsControl.BeginInvoke((MethodInvoker)(() =>
+                {
+                    try
+                    {
+                        if (control is {IsDisposed: true})
+                            return;
+
+                        control.RaiseClientAreaClicked();
+
+                        if (!control.ContainsFocus)
+                            control.Focus();
+                    }
+                    finally
+                    {
+                        _isProcessingClickMessage = false;
+                    }
+                }));
+            }
+            catch
+            {
+                _isProcessingClickMessage = false;
+            }
+        }
+        else
+        {
+            control.RaiseClientAreaClicked();
+            if (!control.ContainsFocus)
+                control.Focus();
+        }
+
         return true;
     }
 }
